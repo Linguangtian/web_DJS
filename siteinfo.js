d29209713ecc9113537fcd02ed086e768a34b27d
lgt <563547352@qq.com>
2019-04-10 11:04:32 +0800

    222

---
+++ siteinfo.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: "小程序商城",
+  uniacid: "6",
+  acid: "6",
+  multiid: "0",
+  version: "2.9.38",
+  siteroot: "https://www.game88.store/app/index.php",
+  apiroot: "https://www.game88.store/web/index.php?store_id=1&r=api/",
+  design_method: "3"
+};
