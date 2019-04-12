8dc2c199b49fd05aef783e3a8098bc4a32d643fd
lgt <563547352@qq.com>
2019-04-12 10:58:43 +0800

    222

--- siteinfo.js
+++ siteinfo.js
@@ -1,10 +1,10 @@
 module.exports = {
   name: "小程序商城",
-  uniacid: "6",
-  acid: "6",
+  uniacid: "5",
+  acid: "5",
   multiid: "0",
   version: "2.9.38",
-  siteroot: "https://www.game88.store/app/index.php",
-  apiroot: "https://www.game88.store/web/index.php?store_id=1&r=api/",
+  siteroot: "https://www.ydproject.tech/app/index.php",
+  apiroot: "https://www.ydproject.tech/web/index.php?store_id=1&r=api/",
   design_method: "3"
 };
